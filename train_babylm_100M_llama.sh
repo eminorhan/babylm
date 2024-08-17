5da81b21f100acb76d5fec35bb9e1425db2e5231
eminorhan <aeminorhan@gmail.com>
2024-08-17 13:03:49 -0400

    add 100M training script

--- train_babylm_100M_llama.sh
+++ train_babylm_100M_llama.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --gres=gpu:a100:4
+#SBATCH --gres=gpu:h100:4
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=480GB
-#SBATCH --time=9:00:00
-#SBATCH --job-name=train_babylm_10M_llama
-#SBATCH --output=train_babylm_10M_llama_%A_%a.out
+#SBATCH --time=48:00:00
+#SBATCH --job-name=train_babylm_100M_llama
+#SBATCH --output=train_babylm_100M_llama_%A_%a.out
 #SBATCH --array=0
 
 export HF_HOME="/vast/eo41/huggingface"
@@ -13,16 +13,16 @@ export HF_DATASETS_CACHE="/vast/eo41/huggingface"
 
 # root model directory
 MODEL_ROOT_DIR="/vast/eo41/babylm/models"
-SP="babylm_10M_llama"
+SP="babylm_100M_llama"
 
 accelerate launch --config_file accelerate_4gpu_config.yaml --num_cpu_threads_per_process 16 /scratch/eo41/babylm/train.py \
     --model_name_or_path "meta-llama/Meta-Llama-3.1-8B" \
-    --train_files "data/text_data/train_10M/childes.txt" \
-                  "data/text_data/train_10M/bnc_spoken.txt" \
-                  "data/text_data/train_10M/gutenberg.txt" \
-                  "data/text_data/train_10M/open_subtitles.txt" \
-                  "data/text_data/train_10M/simple_wiki.txt" \
-                  "data/text_data/train_10M/switchboard.txt" \
+    --train_files "data/text_data/train_100M/childes.txt" \
+                  "data/text_data/train_100M/bnc_spoken.txt" \
+                  "data/text_data/train_100M/gutenberg.txt" \
+                  "data/text_data/train_100M/open_subtitles.txt" \
+                  "data/text_data/train_100M/simple_wiki.txt" \
+                  "data/text_data/train_100M/switchboard.txt" \
     --val_files "data/text_data/dev/childes.txt" \
                 "data/text_data/dev/bnc_spoken.txt" \
                 "data/text_data/dev/gutenberg.txt" \
