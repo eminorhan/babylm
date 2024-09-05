a1c62b605a5f9ed66fc7ea791640f00a7beda0c8
eminorhan <aeminorhan@gmail.com>
2024-09-05 08:32:37 -0400

    update random wikipedia subset creation code

--- create_babylm_dataset.py
+++ create_babylm_dataset.py
@@ -1,5 +1,7 @@
 import random
+import json
 import numpy as np
+from huggingface_hub import HfApi
 from datasets import load_dataset
 
 ds = load_dataset("wikimedia/wikipedia", "20231101.en")
@@ -9,7 +11,7 @@ print('Number of data instances:', n)
 
 for i in range(n):
     lens[i] = len(ds['train'][i]['text'].split())
-    if i % 10000 == 0:
+    if i % 100000 == 0:
         print(f"Article {i}; Length {lens[i]} words.")
 
 print(f"Min words in dataset: {np.min(lens)}")
@@ -19,7 +21,8 @@ print(f"Total words in dataset: {np.sum(lens)}")
 
 def select_items_to_target(y, x, target_length=10000000):
     # Initialize list to hold selected items and their lengths
-    selected_items = []
+    train_articles = []
+    val_articles = []
     current_length_sum = 0
     
     # Create a list of indices to sample from
@@ -34,18 +37,47 @@ def select_items_to_target(y, x, target_length=10000000):
             break
         
         # Add the item and update the sum
-        selected_items.append(y[index])
+        train_articles.append(y[index])
         current_length_sum += x[index]
         
         # Remove the index to avoid picking the same item again
         indices.remove(index)
-        
-    print(f"Total number of articles selected: {len(selected_items)}")
 
-    return {'train': selected_items}
+    # val data
+    for _ in range(1000):
+        index = random.choice(indices)
+        val_articles.append(y[index])
+
+    print(f"Total number of articles selected: {len(train_articles)}")
+    print(f"Total number of words: {current_length_sum}")
+
+    return {'train': train_articles, 'validation': val_articles}
 
 ds_10M = select_items_to_target(ds['train'], lens, target_length=10000000)
 ds_100M = select_items_to_target(ds['train'], lens, target_length=100000000)
 
-ds_10M.push_to_hub("eminorhan/random_wikipedia", "10M")
-ds_100M.push_to_hub("eminorhan/random_wikipedia", "100M")
+# Write the dictionaries to a JSON file
+with open('random_wikipedia_10M.json', 'w') as file:
+    json.dump(ds_10M, file, indent=4)  # indent=4 makes the JSON file more readable
+
+with open('random_wikipedia_100M.json', 'w') as file:
+    json.dump(ds_100M, file, indent=4)
+
+api = HfApi()
+
+# upload data files
+api.upload_file(
+    path_or_fileobj="random_wikipedia_10M.json",
+    path_in_repo="10M/random_wikipedia_10M.json",
+    repo_id="eminorhan/random_wikipedia",
+    repo_type="dataset",
+    token=True
+)
+
+api.upload_file(
+    path_or_fileobj="random_wikipedia_100M.json",
+    path_in_repo="100M/random_wikipedia_100M.json",
+    repo_id="eminorhan/random_wikipedia",
+    repo_type="dataset",
+    token=True
+)
--- create_babylm_dataset.sh
+++ create_babylm_dataset.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16GB
-#SBATCH --time=2:00:00
+#SBATCH --time=3:00:00
 #SBATCH --job-name=create_babylm_dataset
 #SBATCH --output=create_babylm_dataset_%A_%a.out
 
